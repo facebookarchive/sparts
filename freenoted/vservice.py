fbb8218b986ad134ce4f7e38598d2f79fb70bb36
Peter Ruibal <ruibalp@gmail.com>
2012-10-27 15:38:02 -0700

    Service Framework initial commit

---
+++ freenoted/vservice.py
@@ -0,0 +1,93 @@
+import logging
+import sys
+from argparse import ArgumentParser
+import threading
+
+
+class VService(object):
+    DEFAULT_LOGLEVEL = 'DEBUG'
+    TASKS = []
+
+    def __init__(self, ns):
+        self.logger = logging.getLogger(self.name)
+        self.options = ns
+        self.initLogging()
+        self._stop = False
+        self.tasks = []
+
+    def createTasks(self):
+        tasks = self.options.tasks
+        if tasks == []:
+            print "Available Tasks:"
+            for t in self.TASKS:
+                print " - %s" % t.__name__
+            sys.exit(1)
+            
+        elif tasks is None:
+            tasks = [t.__name__ for t in self.TASKS]
+
+        for t in self.TASKS:
+            if t.__name__ in tasks:
+                self.tasks.append(t(self))
+
+    def startTasks(self):
+        for t in self.tasks:
+            t.start()
+
+    def stop(self):
+        self._stop = True
+        for t in self.tasks:
+            t.stop()
+
+    def join(self):
+        try:
+            for t in self.tasks:
+                t.join()
+        except KeyboardInterrupt:
+            self.logger.info('KeyboardInterrupt Received!  Stopping Tasks...')
+            self.stop()
+            for t in self.tasks:
+                t.join()
+
+    @classmethod
+    def initFromCLI(cls):
+        ap = cls._makeArgumentParser()
+        for t in cls.TASKS:
+            t._addArguments(ap)
+        ns = ap.parse_args()
+        cls.initFromOptions(ns)
+
+    @classmethod
+    def initFromOptions(cls, ns):
+        instance = cls(ns)
+        cls.runloop(instance)
+
+    @classmethod
+    def runloop(cls, instance):
+        while not instance._stop:
+            instance.createTasks()
+            instance.startTasks()
+            instance.join()
+
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+    def initLogging(self):
+        logging.basicConfig(level=self.loglevel, stream=sys.stderr)
+
+    @classmethod
+    def _makeArgumentParser(cls):
+        ap = ArgumentParser()
+        ap.add_argument('--tasks', default=None, nargs='*', metavar='TASK',
+                        help='Tasks to run.  Pass without args to see the '
+                             'list.  If not passed, all tasks will be started') 
+        ap.add_argument('--level', default=cls.DEFAULT_LOGLEVEL,
+                        help='Log Level [%(default)s]')
+        ap.add_argument('--dryrun', action='store_true',
+                        help='Run in "dryrun" mode')
+        return ap
+
+    @property
+    def loglevel(self):
+        return getattr(logging, self.options.level)
---
+++ freenoted/vtask.py
@@ -0,0 +1,50 @@
+import logging
+import threading
+
+
+class VTask(object):
+    OPT_PREFIX = None
+
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+    def __init__(self, service):
+        self.service = service
+        self.logger = logging.getLogger('%s.%s' % (service.name, self.name))
+        self.initTask()
+
+    def initTask(self):
+        self.thread = threading.Thread(target=self._runloop)
+
+    def start(self):
+        self.thread.start()
+
+    def stop(self):
+        pass
+
+    def join(self):
+        while self.thread.isAlive():
+            self.thread.join(0.5)
+
+    def _runloop(self):
+        raise NotImplementedError()
+
+    @classmethod
+    def _loptName(self, *args):
+        return '--' + self._optName(*args).replace('_', '-')
+
+    @classmethod
+    def _optName(cls, *args):
+        name = cls.OPT_PREFIX or cls.__name__
+        parts = [name]
+        parts.extend((p.lower() for p in args))
+        return '_'.join(parts)
+
+    def getTaskOption(self, opt, default=None):
+        return getattr(self.service.options,
+                       self._optName(opt), default)
+
+    @classmethod
+    def _addArguments(cls, ap):
+        pass
