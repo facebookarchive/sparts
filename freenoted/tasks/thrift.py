84308c01e6626e48a4f472ccd91cdedaccd7b93e
Peter Ruibal <ruibalp@gmail.com>
2012-10-27 15:38:26 -0700

    Initial commit of thrift server task

---
+++ freenoted/tasks/thrift.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+
+from ..vtask import VTask
+
+from thrift.server.TNonblockingServer import TNonblockingServer
+from thrift.transport.TSocket import TServerSocket
+
+class NBServerTask(VTask):
+    DEFAULT_HOST = '0.0.0.0'
+    DEFAULT_PORT = 0
+    OPT_PREFIX = 'thrift'
+
+    bound_host = bound_port = None
+
+    def initTask(self):
+        super(NBServerTask, self).initTask()
+        self.socket = TServerSocket(self.getTaskOption('host'),
+                                    self.getTaskOption('port'))
+        self.server = TNonblockingServer(
+            self, self.socket, threads=self.getTaskOption('threads'))
+        self.server.prepare()
+        self.bound_host, self.bound_port = \
+            self.server.socket.handle.getsockname()
+        self.logger.info("Server Started on %s:%s",
+                         self.bound_host, self.bound_port)
+
+    def stop(self):
+        self.server.stop()
+
+    def _runloop(self):
+        while not self.server._stop:
+            self.server.serve()
+
+    @classmethod
+    def _addArguments(cls, ap):
+        super(NBServerTask, cls)._addArguments(ap)
+        ap.add_argument(cls._loptName('host'), default=cls.DEFAULT_HOST,
+                        metavar='HOST',
+                        help='Address to bind server to [%(default)s]')
+        ap.add_argument(cls._loptName('port'), type=int, metavar='PORT',
+                        default=cls.DEFAULT_PORT,
+                        help='Port to run server on [%(default)s]')
+        ap.add_argument(cls._loptName('threads'), type=int, default=10,
+                        metavar='N',
+                        help='Server Worker Threads [%(default)s]')
