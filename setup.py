eb8f2d03a16aacc6726e235eb1adf51d9e517963
Peter Ruibal <ruibalp@gmail.com>
2012-11-01 23:02:56 -0700

    Add preliminary setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sparts",
+    version="0.1",
+    packages=find_packages(),
+
+    install_requires=[],
+    author='Peter Ruibal',
+    author_email='ruibalp@gmail.com',
+    license='ISC',
+    keywords='service boostrap daemon thrift tornado',
+    url='http://github.com/fmoo/sparts'
+)
