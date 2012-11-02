1f130297d54594d3dd0f0ecdabd8b4722b2a6818
Peter Ruibal <ruibalp@gmail.com>
2012-11-02 08:45:20 -0700

    version

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 import os.path
 import re
+import imp
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -13,9 +15,13 @@ def read_md_summary(fname):
         return m.group(1)
     return contents
 
+def version():
+    file, pathname, description = imp.find_module('sparts', ['.'])
+    return imp.load_module('sparts', file, pathname, description).__version__
+
 setup(
     name="sparts",
-    version="0.1",
+    version=version(),
     packages=find_packages(),
     description="Build services in python with as little code as possible",
     long_description=read_md_summary("README.md"),
