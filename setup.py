4325a61a3ec37cbb7bdbe7b16d1d36f06f85dffb
Peter Ruibal <ruibalp@gmail.com>
2012-11-02 08:24:16 -0700

    Support descriptions in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup, find_packages
+import os.path
+import re
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def read_md_summary(fname):
+    contents = read(fname)
+    m = re.match('(.*?)\n[^\n]+\n===', contents,
+                 flags=(re.DOTALL | re.MULTILINE))
+    if m:
+        return m.group(1)
+    return contents
 
 setup(
     name="sparts",
     version="0.1",
     packages=find_packages(),
+    description="Build services in python with as little code as possible",
+    long_description=read_md_summary("README.md"),
 
     install_requires=[],
     author='Peter Ruibal',
