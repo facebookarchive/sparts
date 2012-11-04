69b546df0fe93377e420e62b29e0666b558cde19
Peter Ruibal <ruibalp@gmail.com>
2012-11-04 05:51:07 -0800

    Add a "producer consumer" demo using QueueTask and PeriodicTask

---
+++ demo/producer_consumer.py
@@ -0,0 +1,33 @@
+from sparts.tasks.periodic import PeriodicTask
+from sparts.tasks.queue import QueueTask
+from sparts.vservice import VService
+import random
+import threading
+
+
+class Producer(PeriodicTask):
+    INTERVAL = 1.0
+
+    def initTask(self):
+        super(Producer, self).initTask()
+        self.consumer = self.service.requireTask('Consumer')
+
+    def execute(self, *args, **kwargs):
+        for i in xrange(5):
+            item = random.random()
+            self.consumer.queue.put(item)
+            self.logger.info("Producer put %s into queue", item)
+
+class Consumer(QueueTask):
+    WORKERS = 10
+
+    def execute(self, item, context):
+        self.logger.info("[%s] Got %s", threading.current_thread().name, item)
+
+
+class ProducerConsumer(VService):
+    TASKS=[Producer, Consumer]
+
+
+if __name__ == '__main__':
+    ProducerConsumer.initFromCLI()
