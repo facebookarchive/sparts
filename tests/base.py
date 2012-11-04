a9e69025db7bf3c2f3cdf241f7c9b60a1e78ca58
Peter Ruibal <ruibalp@gmail.com>
2012-11-04 04:53:05 -0800

    Support unittests that require multiple tasks

--- tests/base.py
+++ tests/base.py
@@ -1,4 +1,5 @@
 import unittest2
+import logging
 
 from sparts.vservice import VService
 
@@ -7,20 +8,48 @@ class BaseSpartsTestCase(unittest2.TestCase):
     def assertNotNone(self, o, msg=''):
         self.assertTrue(o is not None, msg)
 
-class SingleTaskTestCase(BaseSpartsTestCase):
-    TASK = None
+    def assertNotEmpty(self, o, msg=''):
+        self.assertTrue(len(o) > 0, msg)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.logger = logging.getLogger('sparts.%s' % cls.__name__)
+        super(BaseSpartsTestCase, cls).setUpClass()
+
+class MultiTaskTestCase(BaseSpartsTestCase):
+    def requireTask(self, task_name):
+        self.assertNotNone(self.service)
+        return self.service.requireTask(task_name)
+
+    TASKS = []
     def setUp(self):
-        self.assertNotNone(self.TASK)
+        self.assertNotEmpty(self.TASKS)
 
         class TestService(VService):
-            TASKS=[self.TASK]
+            TASKS=self.TASKS
 
         ap = TestService._makeArgumentParser()
         ns = ap.parse_args(['--level', 'DEBUG'])
         self.service = TestService(ns)
         self.runloop = self.service.startBG()
-        self.task = self.service.requireTask(self.TASK.__name__)
+
+        for t in self.TASKS:
+            self.service.requireTask(t.__name__)
 
     def tearDown(self):
         self.service.stop()
         self.runloop.join()
+
+class SingleTaskTestCase(MultiTaskTestCase):
+    TASK = None
+
+    @classmethod
+    def setUpClass(cls):
+        if cls.TASK:
+            cls.TASKS = [cls.TASK]
+        super(SingleTaskTestCase, cls).setUpClass()
+
+    def setUp(self):
+        self.assertNotNone(self.TASK)
+        MultiTaskTestCase.setUp(self)
+        self.task = self.service.requireTask(self.TASK.__name__)
