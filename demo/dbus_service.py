5a28387c3e26065304c5c435034e39d1cad2f50a
Peter Ruibal <ruibalp@gmail.com>
2012-11-07 09:52:24 -0800

    Add FB303 Processor Task to dbus_service (to prove it works)

--- demo/dbus_service.py
+++ demo/dbus_service.py
@@ -2,13 +2,14 @@ from __future__ import absolute_import
 
 from sparts.vservice import VService
 from sparts.tasks.dbus import DBusMainLoopTask, DBusServiceTask
+from sparts.tasks.fb303 import FB303ProcessorTask
 
 
 class MyDBusServiceTask(DBusServiceTask):
     BUS_NAME = 'org.sparts.DBusDemo'
 
 class MyDBusService(VService):
-    TASKS=[DBusMainLoopTask, MyDBusServiceTask]
+    TASKS=[DBusMainLoopTask, MyDBusServiceTask, FB303ProcessorTask]
 
 
 if __name__ == '__main__':
