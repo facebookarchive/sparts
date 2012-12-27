aab722e1072fe5857ee1f4dbe699676ac3c2c061
Peter Ruibal <ruibalp@gmail.com>
2012-12-26 23:33:21 -0800

    Use threading.Event() to stop PeriodicTasks
    This is a lot more cpu efficient and results in less tasks swapping
    randomly.

--- sparts/tasks/periodic.py
+++ sparts/tasks/periodic.py
@@ -1,6 +1,7 @@
 from ..vtask import VTask
 import time
 from ..sparts import option
+from threading import Event
 
 
 class PeriodicTask(VTask):
@@ -13,18 +14,20 @@ class PeriodicTask(VTask):
     def initTask(self):
         super(PeriodicTask, self).initTask()
         assert self.getTaskOption('interval') is not None
+        self.stop_event = Event()
+
+    def stop(self):
+        self.stop_event.set()
+        super(PeriodicTask, self).stop()
 
     def _runloop(self):
         while not self.service._stop:
-            end_time = time.time() + self.interval
+            t0 = time.time()
             self.execute()
-
-            while not self.service._stop:
-                tn = time.time()
-                to_sleep = end_time - tn
-                if to_sleep <= 0:
-                    break
-                time.sleep(min(0.1, to_sleep))
+            to_sleep = time.time() - (t0 + self.interval)
+            if to_sleep > 0:
+                if self.stop_event.wait(to_sleep):
+                    return
 
     def execute(self, context=None):
         self.logger.debug('execute')
