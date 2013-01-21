38a03aa12adba06e679371ec3c5d5496699ecc5c
Peter Ruibal <ruibalp@gmail.com>
2013-01-20 22:58:26 -0800

    Add a new build rule, "submodule_copy"
    I've replaced the symlinks to externals with actual copies of the files.
    The files are kept up to date automatically by developers who have the
    full development environment configured (with git submodules checked out)
    
    The submodule_copy rule takes a dictionary mapping paths in externals
    to paths where the copy should reside (and stay up to date) in the
    primary source.

--- setup.py
+++ setup.py
@@ -11,37 +11,89 @@ THRIFT = find_executable('thrift')
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
 def read(fname):
+    """Read a file relative to the repository root"""
     return open(os.path.join(ROOT, fname)).read()
 
+def exists(fname):
+    """Returns True if `fname` relative to `ROOT` exists"""
+    return os.path.exists(os.path.join(ROOT, fname))
+
 def version():
+    """Return the version number from sparts/__version__.py"""
     file, pathname, description = imp.find_module('sparts', [ROOT])
     return imp.load_module('sparts', file, pathname, description).__version__
 
-class gen_thrift(Command):
+# Initialize custom command handlers
+cmdclass = {}
+
+# These files are shadowed in the source repository from
+# externals.  If you are developing sparts, you can use git submodule to make
+# sure you have the latest/greatest fb303 from thrift.
+WANT_COPY = {
+    'externals/thrift/contrib/fb303/if/fb303.thrift':
+        'thrift/fb303.thrift', 
+    'externals/thrift/contrib/fb303/py/fb303/FacebookBase.py':
+        'sparts/fb303/FacebookBase.py', 
+}
+
+# Let's figure out which files exist in which submodules...
+CAN_COPY = []
+for src in WANT_COPY:
+    if exists(src):
+        CAN_COPY.append(src)
+
+
+class submodule_copy(Command):
     user_options=[]
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
-        self.mkpath(os.path.join(ROOT, 'sparts', 'gen'))
-        for f in glob(os.path.join(ROOT, 'thrift', '*.thrift')):
-            self.spawn([THRIFT, '-out', os.path.join(ROOT, 'sparts', 'gen'),
-                        '-v', '--gen', 'py:new_style',
-                        os.path.join(ROOT, 'thrift', f)])
+        for src in CAN_COPY:
+            self.copy_file(os.path.join(ROOT, src),
+                           os.path.join(ROOT, WANT_COPY[src]))
 
-class build_py(_build_py):
-    def run(self):
-        self.run_command('gen_thrift')
-        _build_py.run(self)
+if CAN_COPY:
+    cmdclass['submodule_copy'] = submodule_copy
 
-
-cmdclass = {}
 # If we have a thrift compiler installed, let's use it to re-generate
 # the .py files.  If not, we'll use the pre-generated ones.
 if THRIFT is not None:
-    cmdclass = {'gen_thrift': gen_thrift,
-                'build_py': build_py}
+    class gen_thrift(Command):
+        user_options=[]
+
+        def initialize_options(self):
+            pass
+
+        def finalize_options(self):
+            pass
+
+        def run(self):
+            self.mkpath(os.path.join(ROOT, 'sparts', 'gen'))
+            for f in glob(os.path.join(ROOT, 'thrift', '*.thrift')):
+                self.spawn([THRIFT, '-out', os.path.join(ROOT, 'sparts', 'gen'),
+                            '-v', '--gen', 'py:new_style',
+                            os.path.join(ROOT, 'thrift', f)])
+
+    cmdclass['gen_thrift'] = gen_thrift
+
+
+# Custom build_py handler.  Triggers submodule_copy and gen_thrift
+# if the environment is right.
+class build_py(_build_py):
+    def run(self):
+        if CAN_COPY:
+            self.run_command('submodule_copy')
+
+        if THRIFT is not None:
+            self.run_command('gen_thrift')
+        _build_py.run(self)
+
+cmdclass['build_py'] = build_py
 
 VERSION = version()
 setup(
--- sparts/fb303/FacebookBase.py
+++ sparts/fb303/FacebookBase.py
@@ -1,1 +1,77 @@
-../../externals/thrift/contrib/fb303/py/fb303/FacebookBase.py
+#!/usr/bin/env python
+
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+import time
+import FacebookService
+from ttypes import fb_status
+
+class FacebookBase(FacebookService.Iface):
+
+  def __init__(self, name):
+    self.name = name
+    self.alive = int(time.time())
+    self.counters = {}
+    self.options = {}
+
+  def getName(self, ):
+    return self.name
+
+  def getVersion(self, ):
+    return ''
+
+  def getStatus(self, ):
+    return fb_status.ALIVE
+
+  def getStatusDetails(self, ):
+    return ''
+
+  def getCounters(self):
+    return self.counters
+
+  def resetCounter(self, key):
+    self.counters[key] = 0
+
+  def getCounter(self, key):
+    return self.counters.get(key, 0)
+
+  def incrementCounter(self, key):
+    self.counters[key] = self.getCounter(key) + 1
+
+  def setOption(self, key, value):
+    self.options[key] = value
+
+  def getOption(self, key):
+    return self.options[key]
+
+  def getOptions(self):
+    return self.options
+
+  def aliveSince(self):
+    return self.alive
+
+  def getCpuProfile(self, duration):
+    return ""
+
+  def reinitialize(self):
+    pass
+
+  def shutdown(self):
+    pass
