b3549d4673d115f3f24857aea0707abd805ec221
Peter Ruibal <ruibalp@gmail.com>
2013-07-17 13:53:34 -0700

    Add PollerTask
    PollarTask is a subclass of PeriodicTask that abstracts out the common
    operation of periodically checking something and providing a callback
    whenever the value of that something changes.

---
+++ sparts/tasks/poller.py
@@ -0,0 +1,23 @@
+from .periodic import PeriodicTask
+from threading import Event
+
+
+class PollerTask(PeriodicTask):
+    def initTask(self):
+        self.current_value = None
+        self.fetched = Event()
+        super(PollerTask, self).initTask()
+
+    def execute(self, context=None):
+        new_value = self.fetch()
+        if self.current_value != new_value:
+            self.onValueChanged(self.current_value, new_value)
+        self.current_value = new_value
+        self.fetched.set()
+
+    def onValueChanged(self, old_value, new_value):
+        self.logger.debug('onValueChanged(%s, %s)', old_value, new_value)
+
+    def fetch(self):
+        self.logger.debug('fetch')
+        return None
