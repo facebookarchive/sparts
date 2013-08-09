8c1d8a07ad72df9dfef20529e0312fd95f6b2820
Peter Ruibal <ruibalp@gmail.com>
2013-08-08 18:13:43 -0700

    Sexy Counters: "samples"
    This defines an API for declarative sample-based meta counters
    
    That, is you can now do something like:
      from sparts.sparts import samples, SampleType
    
      class MyTask(PeriodicTask):
          interval = 10
          execute_duration = samples(windows=[60, 240], types=[SampleType.AVG])
          def execute(self):
              t0 = time.time()
              # Something expensive
              self.execute_duration.add(time.time() - t0)
    
    This will generate some counters like:
    
      {"MyTask.avg.60": 10.5,
       "MyTask.avg.240": 16.2}

--- sparts/tasks/periodic.py
+++ sparts/tasks/periodic.py
@@ -1,13 +1,17 @@
 from ..vtask import VTask
 import time
-from ..sparts import option, counter
+from ..sparts import option, counter, samples, SampleType
 from threading import Event
 
 
 class PeriodicTask(VTask):
     INTERVAL = None
 
-    iterations = counter()
+    execute_duration = samples(windows=[60, 240],
+       types=[SampleType.AVG, SampleType.MAX, SampleType.MIN])
+    n_iterations = counter()
+    n_slow_iterations = counter()
+
     interval = option(type=float, metavar='SECONDS',
                       default=lambda cls: cls.INTERVAL,
                       help='How often this task should run [%(default)s] (s)')
@@ -25,14 +29,14 @@ class PeriodicTask(VTask):
         while not self.service._stop:
             t0 = time.time()
             self.execute()
-            self.iterations.increment()
+            self.n_iterations.increment()
+            self.execute_duration.add(time.time() - t0)
             to_sleep = (t0 + self.interval) - time.time()
             if to_sleep > 0:
                 if self.stop_event.wait(to_sleep):
                     return
             else:
-                #self.incrementCounter('n_slow_intervals')
-                pass
+                self.n_slow_iterations.increment()
 
     def execute(self, context=None):
         self.logger.debug('execute')
