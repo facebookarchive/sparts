21af3dbed471c9f6c860db4d2ae84d1e0fed4077
Peter Ruibal <ruibalp@gmail.com>
2013-08-08 20:40:30 -0700

    Update option example to highlight samples as well
    And overriding samples

--- demo/option_example.py
+++ demo/option_example.py
@@ -1,6 +1,6 @@
 from sparts.tasks.periodic import PeriodicTask
 from sparts.vservice import VService
-from sparts.sparts import option
+from sparts.sparts import option, samples, SampleType
 import socket
 
 class HostCheckTask(PeriodicTask):
@@ -11,9 +11,21 @@ class HostCheckTask(PeriodicTask):
         self.logger.info("LOOKUP %s => %s", self.check_name,
                          socket.gethostbyname(self.check_name))
 
+class PrintCountersTask(PeriodicTask):
+    INTERVAL=6
+    execute_duration = samples(windows=[60],
+       types=[SampleType.MAX, SampleType.MIN])
+    def execute(self, *args, **kwargs):
+        hostcheck = self.service.requireTask(HostCheckTask)
+        self.logger.info("hostcheck.duration :: %s",
+                         hostcheck.execute_duration.getCounters())
+        self.logger.info("this.duration :: %s",
+                         self.execute_duration.getCounters())
+
+
 
 class DNSChecker(VService):
-    TASKS=[HostCheckTask]
+    TASKS=[HostCheckTask, PrintCountersTask]
 
 
 if __name__ == '__main__':
