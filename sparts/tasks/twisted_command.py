4350014fb052a801bc5cb18c525f6e794504ac92
Peter Ruibal <ruibalp@gmail.com>
2013-08-14 22:09:54 -0700

    Add a task for running commands
    This adds a helper task for running commands asynchronously, registering
    callbacks for stdout, stderr, and process termination.
    
    It uses Twisted, and the callbacks are executed in the context
    of the ioloop, so you have to be careful not to block in there.  Though,
    this could be mitigated by using reactor.callInThread, I am not sure if
    it's worth it.
    
    Technically, this fixes #38

---
+++ sparts/tasks/twisted_command.py
@@ -0,0 +1,133 @@
+from __future__ import absolute_import
+from twisted.internet.protocol import ProcessProtocol
+from twisted.protocols.basic import LineReceiver
+from .twisted import TwistedTask
+from ..sparts import counter, option
+
+import functools
+import signal
+import time
+import twisted.python.threadable
+import twisted.internet.threads
+
+
+class CommandTask(TwistedTask):
+    LOOPLESS = True
+    OPT_PREFIX = 'cmd'
+
+    kill_timeout = option(type=float, default=10.0,
+                          help="Default shutdown kill timeout for outstanding "
+                               "commands [%(default)s]")
+    started = counter()
+    finished = counter()
+
+    def initTask(self):
+        super(CommandTask, self).initTask()
+        self.outstanding = {}
+
+    def procExited(self, on_exit, proto, trans, reason):
+        self.logger.debug("%s closed for %s", trans, reason)
+        if on_exit is not None:
+            on_exit(reason)
+
+        self.outstanding.pop(trans)
+
+        self.finished.increment()
+        return None
+
+    def run(self, command, on_stdout=None, on_stderr=None, on_exit=None,
+            line_buffered=True, kill_timeout=None):
+        self.logger.debug("task starting %s...", command)
+        if isinstance(command, basestring):
+            command = [command]
+
+        # wrap on_exit with helper to remove registered comments
+        on_exit = functools.partial(self.procExited, on_exit)
+
+        proto = _ProcessProtocolAdapter(on_stdout, on_stderr, on_exit,
+                                       line_buffered=line_buffered)
+
+        if twisted.python.threadable.isInIOThread():
+            result = self.reactor.spawnProcess(proto, executable=command[0],
+                                               args=command)
+        else:
+            result = twisted.internet.threads.blockingCallFromThread(
+                self.reactor, self.reactor.spawnProcess, proto,
+                executable=command[0], args=command)
+
+        self.outstanding[result] = kill_timeout
+
+        self.started.increment()
+        return result
+
+    def join(self):
+        """Override to wait for process workers to shutdown / be killed"""
+        # TODO: Conditions instead of sleep polling? 
+        while len(self.outstanding) > 0:
+            time.sleep(0.250)
+
+    def _killOutstanding(self, trans):
+        if trans in self.outstanding:
+            self.logger.info("Sending SIGKILL to %s", trans)
+            trans.signalProcess(signal.SIGKILL)
+
+    def stop(self):
+        # twisted is pretty smart; the default signal handlers it installs
+        # propagate SIGTERM to its children, so while we don't need to manually
+        # TERM, we might still need to set some kill timeouts
+        super(CommandTask, self).stop()
+
+        for trans, kill_timeout in self.outstanding.iteritems():
+            if kill_timeout is None:
+                kill_timeout = self.kill_timeout
+
+            self.logger.info("Enqueuing kill for %s in %.1fs",
+                             trans, kill_timeout)
+            args = (kill_timeout, self._killOutstanding, trans)
+            if twisted.python.threadable.isInIOThread():
+                self.reactor.callLater(*args)
+            else:
+                self.reactor.callFromThread(self.reactor.callLater, *args)
+
+
+class _ProcessProtocolAdapter(ProcessProtocol):
+    def __init__(self, on_stdout=None, on_stderr=None, on_exit=None,
+                 line_buffered=True):
+
+        self.line_buffered = line_buffered
+        self.on_stdout = on_stdout or (lambda *args: None)
+        self.on_stderr = on_stderr or (lambda *args: None)
+        self.on_exit = on_exit or (lambda *args: None)
+
+        if self.line_buffered:
+            self.stdout_buffer = self._makeBuffer(self.on_stdout)
+            self.stderr_buffer = self._makeBuffer(self.on_stderr)
+
+    def _makeBuffer(self, callback):
+        buf = LineReceiver()
+        buf.delimiter = "\n"
+        buf.lineReceived = lambda line: callback(self.transport, line)
+        return buf
+
+    def connectionMade(self):
+        ProcessProtocol.connectionMade(self)
+        self.original_pid = self.transport.pid
+
+    def outReceived(self, data):
+        if self.line_buffered:
+            self.stdout_buffer.dataReceived(data)
+        else:
+            self.on_stdout(self.transport, data)
+
+    def errReceived(self, data):
+        if self.line_buffered:
+            self.stderr_buffer.dataReceived(data)
+        else:
+            self.on_stderr(self.transport, data)
+
+    def processEnded(self, reason):
+        # Ugh, this is a hack... I need to pass self because Twisted unsets pid
+        # after the process has exited, and I need the *old* pid to do proper
+        # accounting.  *YES* I get why this is fundamentally broken
+        self.on_exit(self, self.transport, reason)
+        self.transport.loseConnection()
---
+++ tests/tasks/twisted_command.py
@@ -0,0 +1,46 @@
+from sparts.tasks.twisted_command import CommandTask
+from ..base import SingleTaskTestCase 
+
+import threading
+
+
+class PollerTests(SingleTaskTestCase):
+    TASK = CommandTask
+
+    def test_line_buffered(self):
+        result = ["", None, threading.Event()]
+
+        def on_stdout(trans, msg):
+            result[0] += msg
+
+        def on_exit(reason):
+            result[1] = reason
+            result[2].set()
+
+        self.task.run(['echo', 'Hello World!'], on_stdout=on_stdout,
+                      on_exit=on_exit)
+
+        result[2].wait()
+        self.assertEquals(result[0], 'Hello World!')
+
+    # TODO: Current unittest framework has bad subtest isolation.
+    # Since each test causes a twisted reactor to start/stop,
+    # I can't run more than one test per class file(?)
+    """
+
+    def test_unbuffered(self):
+        result = ["", None, threading.Event()]
+
+        def on_stdout(self, msg):
+            result[0] += msg
+
+        def on_exit(self, reason):
+            result[1] = reason
+            result[2].set()
+
+        self.task.run(['echo', 'Hello World!'], on_stdout=on_stdout,
+                      on_exit=on_exit, line_buffered=False)
+
+        self.assertTrue(result[2].wait(30.0))
+        self.assertEquals(result[0], 'Hello World!')
+    """
