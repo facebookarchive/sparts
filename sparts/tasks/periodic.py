79390fbdcb9eb19574e55fc2b68b3485f8583b6c
Peter Ruibal <ruibalp@gmail.com>
2014-03-24 10:17:42 -0700

    PeriodicTask.execute_duration{->_ms}
    execute_duration is more useful in milliseconds

--- sparts/tasks/periodic.py
+++ sparts/tasks/periodic.py
@@ -7,7 +7,7 @@ from threading import Event
 class PeriodicTask(VTask):
     INTERVAL = None
 
-    execute_duration = samples(windows=[60, 240],
+    execute_duration_ms = samples(windows=[60, 240],
        types=[SampleType.AVG, SampleType.MAX, SampleType.MIN])
     n_iterations = counter()
     n_slow_iterations = counter()
@@ -36,7 +36,7 @@ class PeriodicTask(VTask):
                 continue
 
             self.n_iterations.increment()
-            self.execute_duration.add(time.time() - t0)
+            self.execute_duration_ms.add((time.time() - t0) * 1000)
             to_sleep = (t0 + self.interval) - time.time()
             if to_sleep > 0:
                 if self.stop_event.wait(to_sleep):
