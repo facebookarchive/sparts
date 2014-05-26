53819c3779dd5c579c2bab4b9f48d1bb0c513d13
Peter Ruibal <ruibalp@gmail.com>
2014-05-25 17:36:30 -0700

    sparts.timer: Add a module with some timing helpers.
    I've seen a common pattern that involves a timer that implements the
    context manager protocol and provides timing information.  I'd like to
    use this for tracking samples in `VTask`s, `ExecuteContext`s, and in
    other places without defining the same sets of variables over and over.
    
    New unittests pass

---
+++ sparts/timer.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2014, Facebook, Inc.  All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+#
+"""Sparts module implementing a basic timer class"""
+import time
+from datetime import timedelta
+
+class Timer(object):
+    """Basic Timer class that can be used as a context manager."""
+    def __init__(self):
+        self.start_time = self.end_time = None
+
+    def __enter__(self):
+        """ContextManager protocol enter to start the timer"""
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """ContextManager protocol exit to stop the timer"""
+        self.stop()
+
+    def start(self):
+        """Explicitly start/restart the timer"""
+        self.start_time = self._time()
+
+    def stop(self):
+        """Explicitly stop the timer"""
+        self.end_time = self._time()
+
+    @property
+    def elapsed(self):
+        """Return the duration that the timer was/is active"""
+        # If the timer is not started, return 0.0
+        if self.start_time is None:
+            return 0.0
+
+        # If the timer has not stopped, return the elapsed time until now
+        if self.end_time is None:
+            return self._time() - self.start_time
+
+        # If the timer has stopped, return the difference in times
+        return self.end_time - self.start_time
+
+    def __str__(self):
+        return str(timedelta(seconds=self.elapsed))
+
+    def _time(self):
+        """Private-ish time to help with mocking/unittests"""
+        return time.time()
---
+++ tests/test_timer.py
@@ -0,0 +1,76 @@
+# Copyright (c) 2014, Facebook, Inc.  All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+#
+from sparts.tests.base import BaseSpartsTestCase
+from sparts.timer import Timer
+
+import os
+import sys
+
+
+class TimerTests(BaseSpartsTestCase):
+    def testInstance(self):
+        t = Timer()
+
+        # Mock the timestamp to some fixed value
+        t._time = self.mock.Mock()
+        t._time.return_value = 100.0
+
+        # Start the timer.  Elapsed should remain 0.0 before and after start
+        self.assertEquals(t.elapsed, 0.0)
+        t.start()
+        self.assertEquals(t.elapsed, 0.0)
+
+        # Make sure running time is 5s
+        t._time.return_value = 105.0
+        self.assertEquals(t.elapsed, 5.0)
+
+        # Stop and make sure elapsed time is 5s as well
+        t.stop()
+        self.assertEquals(t.elapsed, 5.0)
+
+        # Bump time by 5s, but make sure elapsed stays the same
+        t._time.return_value = 110.0
+        self.assertEquals(t.elapsed, 5.0)
+
+        # Re-stop the timer, which should update stop_time and elapsed to 10s
+        t.stop()
+        self.assertEquals(t.elapsed, 10.0)
+
+    def testContext(self):
+        with Timer() as t:
+            # Mock the timestamp to some fixed value
+            t._time = self.mock.Mock()
+            t._time.return_value = 100.0
+
+            # Context manager should have started the timer, so elapsed
+            # should be some big negative number
+            self.assertLess(t.elapsed, 0)
+
+            # Restart the timer, so we get good data
+            t.start()
+
+            # Start the timer.  Elapsed should remain 0.0 before and after start
+            t.start()
+            self.assertEquals(t.elapsed, 0.0)
+
+            # Make sure running time is 3s
+            t._time.return_value = 103.0
+            self.assertEquals(t.elapsed, 3.0)
+
+            # Bump time again, before exiting
+            t._time.return_value = 105.0
+
+        # Stop (via exit) and make sure elapsed time is 5s as well
+        self.assertEquals(t.elapsed, 5.0)
+
+        # Bump time by 5s, but make sure elapsed stays the same
+        t._time.return_value = 110.0
+        self.assertEquals(t.elapsed, 5.0)
+
+        # Re-stop the timer, which should update stop_time and  elapsed to 10s
+        t.stop()
+        self.assertEquals(t.elapsed, 10.0)
