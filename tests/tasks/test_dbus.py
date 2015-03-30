6d24dedb75718647af98547e9b367a5bb9200f03
Maciek Borzecki <maciek.borzecki@gmail.com>
2015-03-30 16:24:33 +0200

    dbus: minimal test case for dbus tasks

---
+++ tests/tasks/test_dbus.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2015, Facebook, Inc.  All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+#
+from sparts.tasks.dbus import DBusServiceTask, DBusMainLoopTask
+from sparts.tests.base import MultiTaskTestCase
+from sparts.sparts import option
+import dbus
+
+class TestDBusTask(DBusServiceTask):
+    BUS_NAME = 'com.github.facebook.test'
+
+
+class TestDBusSystemTask(DBusServiceTask):
+    BUS_NAME = 'com.github.facebook.systemtest'
+
+    system_bus = option(name='system_bus', default=True)
+
+    def start(self):
+        try:
+            super(TestDBusSystemTask, self).start()
+        except dbus.DBusException as err:
+            self.acquire_name_error = str(err)
+
+
+class TestDBus(MultiTaskTestCase):
+    TASKS = [TestDBusTask, DBusMainLoopTask]
+
+    def test_session_bus(self):
+        bus = dbus.SessionBus(private=True)
+        self.assertTrue(bus.name_has_owner(TestDBusTask.BUS_NAME))
+
+
+class TestSystemDBus(MultiTaskTestCase):
+    TASKS = [TestDBusSystemTask, DBusMainLoopTask]
+
+    def test_system_bus(self):
+        # expecting system task to fail due to missing DBus policy
+        # file
+        t = self.service.getTask(TestDBusSystemTask)
+        err = getattr(t, 'acquire_name_error', None)
+        self.assertNotNone(err)
+
+        self.assertTrue(err.startswith('org.freedesktop.DBus.Error.AccessDenied'))
+
+
